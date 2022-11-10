bee9b375f92bfe8f9ddda275d042ad6079cb49b2
DiegoEstela <die.estela@gmail.com>
2022-11-10 20:04:07 +0100

    index

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const express = require('express')
+const express = require("express");
+const app = express();
+const port = 8000;
+
+
+app.get("/", (req, res) => {
+  res.send("Bienvenido a mi API");
+});
+
+
+app.listen(port, () => {
+  console.log("server run on port " + port);
+});
