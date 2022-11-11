4e8c9f6c1ace85669df76848bdedb445220b833a
DiegoEstela <die.estela@gmail.com>
2022-11-11 13:11:10 +0100

    user

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
+const cors = require('cors')
 
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const users = [
+  {email: "diego@gmail.com",
+  fullName: "diego estela"},
+  {email: "eze@gmail.com",
+  fullName: "ezequiel prueba"}
+]
 
 
 app.get("/", (req, res) => {
@@ -13,8 +24,8 @@ app.get("/file", (req, res) => {
   });
 
 
-app.get("/files", (req, res) => {
-    res.send("Bienvenido a mi files");
+app.get("/users", (req, res) => {
+    res.send(users);
   });
 
 app.listen(port, () => {
